0a2cfc5273b594410b3d1ba788bb1d7d2e69c28f
Bryan Vera <bveracachay@gmail.com>
2025-03-22 12:00:32 -0500

    se crea tabla faltante

--- models/regalo.model.js
+++ models/regalo.model.js
@@ -1,44 +1,45 @@
-import { pool } from "../database/connection.js";
+import { pool } from '../database/connection.js';
 
 const findAll = async () => {
-  const result = await pool.query("SELECT * FROM registroregalos");
-  return result?.rows;
+	const result = await pool.query('SELECT * FROM registroregalos');
+	return result?.rows;
 };
 const findAllRegalos = async () => {
-  const result = await pool.query("SELECT * FROM regalos where estado = true");
-  return result?.rows;
+	const result = await pool.query('SELECT * FROM regalos where estado = true');
+	console.log('aquiii: ' + result.rows);
+	return result?.rows;
 };
 
 const createRegalo = async (regalo) => {
-  const findEstado = await pool.query(
-    "SELECT * FROM regalos where nombre = $1",
-    [regalo.regalo]
-  );
-  console.log(findEstado.rows[0].estado);
-  if (findEstado.rows[0].estado == false) {
-    return { error: "El regalo ya fue seleccionado" };
-  }
-  const result = await pool.query(
-    "INSERT INTO registroregalos ( nombres,apellidos,regalo) VALUES ($1, $2, $3) RETURNING *",
-    [regalo.nombres, regalo.apellidos, regalo.regalo]
-  );
-  await pool.query("UPDATE regalos set estado = false where nombre=$1", [
-    regalo.regalo,
-  ]);
-  return result.rows[0];
+	const findEstado = await pool.query(
+		'SELECT * FROM regalos where nombre = $1',
+		[regalo.regalo]
+	);
+	console.log(findEstado.rows[0].estado);
+	if (findEstado.rows[0].estado == false) {
+		return { error: 'El regalo ya fue seleccionado' };
+	}
+	const result = await pool.query(
+		'INSERT INTO registroregalos ( nombres,apellidos,regalo) VALUES ($1, $2, $3) RETURNING *',
+		[regalo.nombres, regalo.apellidos, regalo.regalo]
+	);
+	await pool.query('UPDATE regalos set estado = false where nombre=$1', [
+		regalo.regalo,
+	]);
+	return result.rows[0];
 };
 
 const updateRegalo = async (id) => {
-  const result = await pool.query(
-    "UPDATE regalos set estado = 0 where id = $1",
-    [id]
-  );
-  return result.rows[0];
+	const result = await pool.query(
+		'UPDATE regalos set estado = 0 where id = $1',
+		[id]
+	);
+	return result.rows[0];
 };
 
 export const regaloModel = {
-  findAll,
-  createRegalo,
-  updateRegalo,
-  findAllRegalos,
+	findAll,
+	createRegalo,
+	updateRegalo,
+	findAllRegalos,
 };
